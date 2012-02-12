3fe0492e41c16c75612e0714f7daa2070d30c78a
Андрей Хитрин <ahitrin@naumen.ru>
2012-02-12 14:27:07 +0600

    Use xdg-open on Linux to fix #5
    Open ftp and http(s) links in programs defined by current user instead
    of hardcoded gftp/firefox.
    
    See [Freedsktop wiki](http://portland.freedesktop.org/wiki/) for further
    information.

--- src/ru/naumen/servacc/platform/Linux.java
+++ src/ru/naumen/servacc/platform/Linux.java
@@ -17,12 +17,12 @@ public class Linux implements Platform
 
     @Override
     public void openFTPBrowser(Object[] params) throws IOException {
-        Runtime.getRuntime().exec(MessageFormat.format("gftp ftp://anonymous@{0}:{1,number,#}", params));
+        Runtime.getRuntime().exec(MessageFormat.format("xdg-open ftp://anonymous@{0}:{1,number,#}", params));
     }
 
     @Override
     public void openInBrowser(String url) throws IOException {
-        Runtime.getRuntime().exec("firefox " + url);
+        Runtime.getRuntime().exec("xdg-open " + url);
     }
 
     @Override
