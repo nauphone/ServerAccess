d92810f04776bf12f43dc88714bdb85902c00563
Andrey Hitrin <andrey.hitrin@gmail.com>
2012-11-25 22:24:04 +0600

    Clear global through info correctly
    Fix #21

--- src/ru/naumen/servacc/ui/UIController.java
+++ src/ru/naumen/servacc/ui/UIController.java
@@ -424,6 +424,7 @@ public class UIController
         globalThrough.setForeground(gray);
         clearGlobalThrough.setVisible(false);
         globalThroughUniqueIdentity = null;
+        backend.setGlobalThrough(null);
     }
 
     private void createTrayItem()
