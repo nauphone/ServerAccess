463030094adc669671143b5b7273a3fd10d1afb9
Andrey Hitrin <andrey.hitrin@gmail.com>
2012-12-23 14:14:36 +0600

    Remove unused import

--- src/ru/naumen/servacc/ui/UIController.java
+++ src/ru/naumen/servacc/ui/UIController.java
@@ -39,7 +39,6 @@ import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.events.ShellListener;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
