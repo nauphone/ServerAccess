2512b77be98c923358d96b6c297ad58b5164e41d
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-01-13 21:17:35 +0600

    Add tests on TreeItemController

--- test/ru/naumen/servacc/test/ui/TreeItemControllerTest.java
+++ test/ru/naumen/servacc/test/ui/TreeItemControllerTest.java
@@ -10,9 +10,15 @@
 package ru.naumen.servacc.test.ui;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+
+import ru.naumen.servacc.config2.HTTPAccount;
+import ru.naumen.servacc.config2.i.IConfigItem;
 import ru.naumen.servacc.ui.TreeItemController;
 
 /**
@@ -21,10 +27,133 @@ import ru.naumen.servacc.ui.TreeItemController;
  */
 public class TreeItemControllerTest
 {
+    private TreeItemController controller = new TreeItemController();
+
     @Test
-    public void doesntMatchStringByDefault()
+    public void defaultParentIsNull()
     {
-        TreeItemController controller = new TreeItemController();
-        assertThat(controller.matches("some string"), is(false));
+        assertThat(controller.getParent(), is(nullValue()));
+    }
+
+    @Test
+    public void parentMustBeSetInConstructor()
+    {
+        TreeItemController parent = new TreeItemController();
+        controller = new TreeItemController(parent, null);
+        assertThat(controller.getParent(), is(parent));
+    }
+
+    @Test
+    public void dataMustBeSetInConstructor()
+    {
+        IConfigItem data = new HTTPAccount();
+        controller = new TreeItemController(null, data);
+        assertThat(controller.getData(), is(data));
+    }
+
+    @Test
+    public void defaultChildrenListIsEmpty()
+    {
+        assertThat(controller.getChildren().size(), is(0));
+    }
+
+    @Test
+    public void visibleByDefault()
+    {
+        assertThat(controller.isVisible(), is(true));
+    }
+
+    @Test
+    public void visibilityCanBeChanged()
+    {
+        controller.setVisibility(false);
+        assertThat(controller.isVisible(), is(false));
+        controller.setVisibility(true);
+        assertThat(controller.isVisible(), is(true));
+    }
+
+    @Test
+    public void collapsedByDefault()
+    {
+        assertThat(controller.isExpanded(), is(false));
+    }
+
+    @Test
+    public void expandedStatusCanBeChanged()
+    {
+        controller.setExpanded(true);
+        assertThat(controller.isExpanded(), is(true));
+        controller.setExpanded(false);
+        assertThat(controller.isExpanded(), is(false));
+    }
+
+    @Test
+    public void doesNotMatchStringByDefault()
+    {
+        assertThat(controller.matches(Arrays.asList("some string")), is(false));
+    }
+
+    @Test
+    public void matchesCollectionWhenDataAcceptsAllStrings()
+    {
+        IConfigItem matchingData = new IConfigItem()
+        {
+            @Override
+            public boolean matches(String filter)
+            {
+                return true;
+            }
+
+            @Override
+            public String getIconName()
+            {
+                return null;
+            }
+        };
+        controller = new TreeItemController(null, matchingData);
+        assertThat(controller.matches(Arrays.asList("one", "two", "three")), is(true));
+    }
+
+    @Test
+    public void mayMatchWhenParentMatchesString()
+    {
+        IConfigItem matchingData = new IConfigItem()
+        {
+            @Override
+            public boolean matches(String filter)
+            {
+                return "should match".equals(filter);
+            }
+
+            @Override
+            public String getIconName()
+            {
+                return null;
+            }
+        };
+        TreeItemController parent = new TreeItemController(null, matchingData);
+        controller = new TreeItemController(parent, new HTTPAccount());
+        assertThat(controller.matches(Arrays.asList("should match")), is(true));
+    }
+
+    @Test
+    public void takesImageNameFromData()
+    {
+        IConfigItem data = new IConfigItem()
+        {
+            @Override
+            public boolean matches(String filter)
+            {
+                return false;
+            }
+
+            @Override
+            public String getIconName()
+            {
+                return "icon name";
+            }
+        };
+        controller = new TreeItemController(null, data);
+        assertThat(controller.getImageName(), is("icon name"));
     }
 }
