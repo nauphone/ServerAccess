8e5ff30fa3d62072cc072b04d75c160c59c8452e
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-01-20 20:26:10 +0600

    Add missing copyrights

--- src/ru/naumen/servacc/platform/Windows.java
+++ src/ru/naumen/servacc/platform/Windows.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2005-2012 NAUMEN. All rights reserved.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.
+ */
 package ru.naumen.servacc.platform;
 
 import java.io.File;
