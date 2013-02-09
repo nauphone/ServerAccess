aaa60c6b6a5b607c9a0ed7252c1135cd69e986e7
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-02-09 22:44:32 +0600

    Create class XTerm

---
+++ src/ru/naumen/servacc/platform/XTerm.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2005-2013 NAUMEN. All rights reserved.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.
+ */
+
+package ru.naumen.servacc.platform;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Map;
+
+/**
+ * @author Andrey Hitrin
+ * @since 03.02.13
+ */
+public class XTerm implements Terminal
+{
+    @Override
+    public void connect(int localPort, Map<String, String> params) throws IOException
+    {
+        new ProcessBuilder("xterm", "-e", MessageFormat.format("telnet 127.0.0.1 {0,number,#}", localPort)).start();
+    }
+}
