f8190f03b0cfd7e919f144a881a520b5a30249a6
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-02-10 01:22:51 +0600

    Add info on terminals into default properties file

--- src/ru/naumen/servacc/settings/impl/DefaultPropertiesFile.java
+++ src/ru/naumen/servacc/settings/impl/DefaultPropertiesFile.java
@@ -12,6 +12,7 @@ package ru.naumen.servacc.settings.impl;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import ru.naumen.servacc.FileResource;
 
@@ -26,6 +27,16 @@ public class DefaultPropertiesFile implements DefaultFile
     {
         File accountsFile = new File(configFile.getParentFile(), "accounts.xml");
         String path = accountsFile.getCanonicalPath().replaceAll("\\\\", "/");
-        new FileOutputStream(configFile).write(("source=" + FileResource.uriPrefix + path).getBytes("UTF-8"));
+        PrintStream outputStream = new PrintStream(new FileOutputStream(configFile));
+        outputStream.println("# You can define several sources, with different number each");
+        outputStream.println("source=" + FileResource.uriPrefix + path);
+        outputStream.println("#source1=<some another source file or url here>");
+        outputStream.println();
+        outputStream.println("# Use 'terminal' variable to set custom terminal launcher.");
+        outputStream.println("# Seek for more documentation on http://github.com/apatrushev/ServerAccess");
+        outputStream.println("#terminal=xterm  {options}  -e  telnet {host} {port}");
+        outputStream.println("#terminal=putty  {options}  -telnet  {host}  -P  {port}");
+        outputStream.println("#terminal=open  telnet://{host}:{port}");
+        outputStream.close();
     }
 }
