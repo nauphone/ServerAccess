8829012a345ecb548159ab1d07847a87a498040f
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-05-15 17:29:14 +0500

    Fix: background task must not freeze its thread

--- src/main/java/ru/naumen/servacc/activechannel/tasks/ActualizeActiveChannelsTask.java
+++ src/main/java/ru/naumen/servacc/activechannel/tasks/ActualizeActiveChannelsTask.java
@@ -37,7 +37,7 @@ public class ActualizeActiveChannelsTask extends Thread
             }
             catch (InterruptedException e)
             {
-                // Nothing to do
+                Thread.currentThread().interrupt();
             }
 
             registry.actualizeAllChannels();
